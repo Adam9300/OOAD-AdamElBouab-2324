3fd021632efbec86ba27636c61811ef77a489d0f
Adam9300 <adam.elbouab@student.odisee.be>
2024-02-21 23:04:16 +0100

    oefening week 1

---
+++ SlnLes01HerhalingAanvulling/ConsoleComplexiteit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+            Console.WriteLine("Welkom bij de Woord Complexiteit Applicatie!");
+            Console.WriteLine("Voer een woord in om de complexiteit te bepalen.");
+            Console.WriteLine("Druk op enter om te stoppen.");
+           
+
+       
+
+            
+
+
+
+
+
+
+        }
+}
