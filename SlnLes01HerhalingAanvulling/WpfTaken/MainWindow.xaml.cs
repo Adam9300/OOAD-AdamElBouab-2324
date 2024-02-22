142a7807d29ec95ccf2b693ad58eca78a594d776
Adam9300 <146444394+Adam9300@users.noreply.github.com>
2024-02-22 19:43:43 +0100

    Update MainWindow.xaml.cs

--- SlnLes01HerhalingAanvulling/WpfTaken/MainWindow.xaml.cs
+++ SlnLes01HerhalingAanvulling/WpfTaken/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-
 namespace WpfTaken
 {
     public partial class MainWindow : Window
